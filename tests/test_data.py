52deb9af6ee90b446fe20943990c24d7ea0462cd
MrYriy <lisa99nirva@gmail.com>
2026-02-06 14:20:22 +0300

    Комментарии по tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,15 @@
-import pandas as pd
-from src.data import load_data
+"""
+Код Ревью:
+- отсутствует стат типизация
+"""
 
+import pandas as pd # излишний импорт
+from src.data import load_data
+# добавить еще одину пустую строку
 def test_load_data():
+    """ """
     df = load_data("data/uber.csv")
     assert not df.empty
+
+# нужен тест на логику предобработки
+# нужен тест на обработку пограничных условий в данных
