b43e9b4bb30e446f3a833340f3643e33a0c0cd69
MrYriy <lisa99nirva@gmail.com>
2026-02-06 14:17:11 +0300

    Комментарии по data

--- src/data.py
+++ src/data.py
@@ -1,9 +1,17 @@
-import pandas as pd
+"""
+Код Ревью:
+- структура каталогов src/data/data.py
+- отсутствует стат типизация
+"""
 
+import pandas as pd
+# добавить еще одину пустую строку
 def load_data(path):
+    """ """
     return pd.read_csv(path)
 
 def split_data(data):
+    """ """
     from sklearn.model_selection import train_test_split
     features = data.drop('fare_amount', axis=1)
     target = data['fare_amount']
