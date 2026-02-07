1e8f9b135f57e70c76d0f1888ae73f80cceaaed7
MrYriy <lisa99nirva@gmail.com>
2026-02-07 12:50:03 +0300

    Revert "Коммит"
    This reverts commit 373a3634ffd2f7ac18dc10d80f48a404d5cff112.

--- src/data.py
+++ src/data.py
@@ -4,7 +4,6 @@ def load_data(path):
     return pd.read_csv(path)
 
 def split_data(data):
-    ''' '''
     from sklearn.model_selection import train_test_split
     features = data.drop('fare_amount', axis=1)
     target = data['fare_amount']
